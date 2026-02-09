418d7c40cf9429edc73ef254864be6e7661f461e
Nitin Zain <nitinshaukia@gmail.com>
2026-02-09 22:16:32 +0530

    Test update

--- src/pages/FeedbackSurvey.jsx
+++ src/pages/FeedbackSurvey.jsx
@@ -38,7 +38,7 @@ const FeedbackSurvey = () => {
           <img
             src={imf}
             alt="Logo Xpress Inn"
-          style={{height:"220px", width:"205px",paddingTop:"5px"}}
+            style={{ height: "220px", width: "205px", paddingTop: "5px" }}
 
           />
         </div>
